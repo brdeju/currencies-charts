d4bb5285217108b9d9c8b4456db99c137f977a07
Grzegorz Brdej <grzebr@o2.pl>
2015-07-24 20:02:21 +0200

    init project

---
+++ README.md
@@ -0,0 +1,4 @@
+CurrenciesCharts
+================
+
+A Symfony project created on July 24, 2015, 4:29 pm.
---
+++ src/Brdeju/Bundle/CurrenciesChartsBundle/CurrenciesChartsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Brdeju\Bundle\CurrenciesChartsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CurrenciesChartsBundle extends Bundle
+{
+}
